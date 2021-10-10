8921f4f515e34b2ac110477eff912930e415798f
Park, Jeongmin <pjm0616@gmail.com>
2021-10-10 15:05:42 +0900

    use foo_beefweb to control fb2k if available

--- scripts/my_playerctl.py
+++ scripts/my_playerctl.py
@@ -10,6 +10,13 @@ The wrapper script should look like this:
 """
 import sys
 import subprocess
+import requests
+import urllib.parse
+
+# Use foo_beefweb component for controlling fb2k. (https://github.com/hyperblast/beefweb)
+# Set to None to disable beefweb and fallback to fb2k command line.
+# To use, 1) install foo_beefweb component, and 2) configure beefweb to listen on 127.0.0.1:12321 without auth.
+fb2k_beefweb_url = 'http://127.0.0.1:12321'
 
 def fb2k_active():
 	"""Checks whether fb2k's status is Playing or Paused"""
@@ -35,18 +42,85 @@ fb2k_cmdmap = {
 	'previous': '/prev',
 
 	# Note: this command is not present in playerctl - it's fb2k only.
-	#'rand': '/rand',
+	'random': '/rand',
 }
-def fb2k_cmd(cmd):
+fb2k_apimap = {
+	'play': '/play',
+	'pause': '/pause',
+	'play-pause': '/pause/toggle',
+	'stop': '/stop',
+	'next': '/next',
+	'previous': '/previous',
+
+	# Note: this command is not present in playerctl - it's fb2k only.
+	'random': '/play/random',
+}
+def fb2k_beefweb_cmd(cmd):
+	assert fb2k_beefweb_url
+	path = fb2k_apimap[cmd]
+	try:
+		r = requests.post('%s/api/player%s' % (fb2k_beefweb_url, path), timeout=0.5)
+	except requests.ConnectionError as err:
+		return False
+	if not r.ok:
+		return False
+	if r.status_code == 200:
+		return r.json()
+
+def fb2k_beefweb_status():
+	assert fb2k_beefweb_url
+	try:
+		r = requests.get('%s/api/player' % fb2k_beefweb_url, timeout=0.5)
+	except requests.ConnectionError as err:
+		return False
+	if r.status_code != 200:
+		return False
+	data = r.json()
+	status = data['player']['playbackState']
+	status = {'playing': 'Playing', 'paused': 'Paused', 'stopped': 'Stopped'}[status]
+	print(status)
+
+def fb2k_beefweb_metadata():
+	assert fb2k_beefweb_url
+	cols = ['album', 'artist', 'title', 'length']
+	colreqstr = ','.join('%'+col+'%' for col in cols)
+	try:
+		r = requests.get('%s/api/player?columns=%s' % (fb2k_beefweb_url, urllib.parse.quote(colreqstr)), timeout=0.5)
+	except requests.ConnectionError as err:
+		return False
+	if r.status_code != 200:
+		return False
+	data = r.json()
+	active = data['player']['activeItem']
+	metadata = dict(zip(cols, active['columns']))
+	player_name = 'foobar2000.exe'
+	for key in ['album', 'artist', 'title']:
+		val = metadata.get(key, '')
+		print('%s xesam:%s %s' % (player_name, key, val))
+
+def fb2k_native_cmd(cmd):
 	fb2k_cmd = fb2k_cmdmap[cmd]
 	subprocess.check_output(['foobar2000', fb2k_cmd], stderr=subprocess.DEVNULL, encoding='utf8')
 
+def fb2k_cmd(cmd):
+	if cmd == 'status':
+		return fb2k_beefweb_status()
+	if cmd == 'metadata':
+		return fb2k_beefweb_metadata()
+
+	ret = False
+	if fb2k_beefweb_url:
+		ret = fb2k_beefweb_cmd(cmd)
+	if ret is False:
+		ret = fb2k_native_cmd(cmd)
+	return ret
+
 if __name__ == '__main__':
 	cmd = sys.argv[1]
-	assert cmd in ['play', 'pause', 'play-pause', 'stop', 'next', 'previous']
+	assert cmd in ['play', 'pause', 'play-pause', 'stop', 'next', 'previous', 'status', 'metadata']
 	if fb2k_active():
 		fb2k_cmd(cmd)
 		if mpris_cmd('status') == 'Playing':
 			mpris_cmd('pause')
 	else:
-		mpris_cmd(cmd)
+		print(mpris_cmd(cmd))
